7bd6d24f7a657ddad731d47e11224162666b5d7d
Gerard Sole <gerardsole@MacBook-Pro-de-Gerard-2.local>
2020-03-14 17:25:33 +0100

    Hacktivities Model added

---
+++ models/Hacktivity.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const hacktivitySchema = new Schema(
+  {
+    host : {type: Schema.Types.ObjectId, ref: 'User' },
+    name: {type: String, required: true },
+    description: {type: String },
+    date: {type: Date },
+    location: {type: Schema.Types.ObjectId, ref: 'City' },
+    duration: {type: Number, min: 0 },
+    created: { type: Date, default: Date.now }
+  }
+);
+
+const Hacktivity = mongoose.model('City', hacktivitySchema);
+
+module.exports = Hacktivity;
