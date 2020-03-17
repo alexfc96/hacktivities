391075cde41e14c07839025c075fed0edcec7c36
Gerard Sole <gerardsole@MacBook-Pro-de-Gerard-2.local>
2020-03-17 19:40:23 +0100

    update 17-03

--- models/Booking.js
+++ models/Booking.js
@@ -7,7 +7,9 @@ const bookingSchema = new Schema(
     hacktivityId: { type: Schema.Types.ObjectId, ref: 'Hacktivity' },
     // nose que mas poner la verdad
     userId: { type: Schema.Types.ObjectId, ref: 'User' },
-    review: { type: String },
+    review: { type: Schema.Types.ObjectId, ref: 'Review' },
+    date: { type: Date },
+    atendees: { type: Array },
   },
 );
 
